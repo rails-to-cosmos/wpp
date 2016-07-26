0c5304272ae3b576bc5801bfa5eead61d16ac958
Rails to Cosmos <akatovda@yandex.com>
2016-07-26 13:54:22 +0300

    Implement filters module.

--- components/actions/ActionDownload.js
+++ components/actions/ActionDownload.js
@@ -1,5 +1,6 @@
 var Action = require('./Action'),
-    Webpage = require('../webpage/Webpage');
+    Webpage = require('../webpage/Webpage'),
+    Filters = require('../webpage/Filters');
 
 function ActionDownload() {
   Action.apply(this, Array.prototype.slice.call(arguments));
@@ -27,45 +28,8 @@ ActionDownload.prototype.main = function() {
     webpage.create().then((page) => {
       var filters = ACTION.get_filters();
       if (filters) {
-        page.property('onResourceRequested', function(requestData, networkRequest, filters) {
-          var url = requestData.url;
-
-          for (var filter_name in filters) {
-            if (filter_name == 'WhitelistUrlFilter') {
-              var url_in_wl = false;
-              var re;
-
-              for (var wlre_index in filters[filter_name].urls) {
-                var wlre = filters[filter_name].urls[wlre_index];
-                re = new RegExp(wlre);
-
-                if (re.test(url)) {
-                  url_in_wl = true;
-                }
-              }
-
-              if(!url_in_wl && filters[filter_name] && filters[filter_name].urls) {
-                console.log('Abort (wl, url): ' + requestData.url);
-                networkRequest.abort();
-                return;
-              }
-            } else if (filter_name == 'BlacklistUrlFilter') {
-              for (var blre_index in filters[filter_name].urls) {
-                var blre = filters[filter_name].urls[blre_index];
-                re = new RegExp(blre);
-
-                if (re.test(url)) {
-                  console.log('Abort (bl, url): ' + requestData.url);
-                  networkRequest.abort();
-                  return;
-                }
-              }
-            }
-          }
-
-          console.log('Accept: ' + url);
-        }, filters);
-      } // if filters
+        Filters.applyFiltersOnPage(page, filters);
+      }
 
       page.open(ACTION.get_url()).then((status) => {
         page.property('content').then((content) => {
---
+++ components/webpage/Filters.js
@@ -0,0 +1,45 @@
+function applyFiltersOnPage(page, filters) {
+  page.property('onResourceRequested', function(requestData, networkRequest, filters) {
+    var url = requestData.url;
+
+    for (var filter_name in filters) {
+      if (filter_name == 'WhitelistUrlFilter') {
+        var url_in_wl = false;
+        var re;
+
+        for (var wlre_index in filters[filter_name].urls) {
+          var wlre = filters[filter_name].urls[wlre_index];
+          re = new RegExp(wlre);
+
+          if (re.test(url)) {
+            url_in_wl = true;
+          }
+        }
+
+        if(!url_in_wl && filters[filter_name] && filters[filter_name].urls) {
+          console.log('Abort (wl, url): ' + requestData.url);
+          networkRequest.abort();
+          return;
+        }
+      } else if (filter_name == 'BlacklistUrlFilter') {
+        for (var blre_index in filters[filter_name].urls) {
+          var blre = filters[filter_name].urls[blre_index];
+          re = new RegExp(blre);
+
+          if (re.test(url)) {
+            console.log('Abort (bl, url): ' + requestData.url);
+            networkRequest.abort();
+            return;
+          }
+        }
+      }
+    }
+
+    console.log('Accept: ' + url);
+  }, filters);
+}
+
+
+module.exports = {
+  applyFiltersOnPage: applyFiltersOnPage
+};
