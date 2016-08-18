17d8d4a324d3e618ef873405b04da748ecee41f7
Rails to Cosmos <akatovda@yandex.com>
2016-08-18 14:39:04 +0300

    Catch click error.

--- components/actions/ActionClickOneElement.js
+++ components/actions/ActionClickOneElement.js
@@ -18,21 +18,50 @@ ActionClickOneElement.prototype.main = function (subactions) {
   return new Promise(function(resolveAllPages) {
     var actions = pages.map(function(page) {
       return new Promise(function(resolveClick) {
+        var load_started = false;
+
+        page.off('onLoadStarted');
+        page.on('onLoadStarted', function(status) {
+          load_started = true;
+        });
+
         page.off('onLoadFinished');
         page.on('onLoadFinished', function(status) {
-          ACTION.run_subactions(subactions).then(function(result) {
-            resolveClick(result);
-          });
+          if (load_started) {
+            ACTION.run_subactions(subactions).then(function(result) {
+              resolveClick(result);
+            });
+          }
         });
+
+        // page.off('onNavigationRequested');
+        // page.on('onNavigationRequested', function(url, type, willNavigate, main) {
+        //   console.log('Trying to navigate to: ' + url);
+        //   console.log('Caused by: ' + type);
+        //   console.log('Will actually navigate: ' + willNavigate);
+        //   console.log('Sent from the page\'s main frame: ' + main);
+        // });
 
         var xpath_module = new XPathInjection();
         var click_module = new ClickInjection();
         xpath_module.apply(page).then(function() {
           click_module.apply(page).then(function() {
             page.evaluate(function(path) {
-              var element = __wpp__.get_element_by_xpath(path);
-              __wpp__.click(element);
-            }, path);
+              try {
+                var element = __wpp__.get_element_by_xpath(path);
+                __wpp__.click(element);
+              } catch (err) {
+                return false;
+              }
+              return Boolean(element);
+            }, path).then(function (result) {
+              if (!result) {
+                console.error('Unable to click on ', path, ACTION.config.name, '->', ACTION.config.target);
+                ACTION.run_subactions(subactions).then(function(result) {
+                  resolveClick(result);
+                });
+              }
+            });
           });
         });
       });
