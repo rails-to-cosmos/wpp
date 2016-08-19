6cf21edef13c28cfca2cb2e6bf7cf4daafd8b762
Rails to Cosmos <akatovda@yandex.com>
2016-08-19 16:27:31 +0300

    Change port to 9263.

--- cluster.js
+++ cluster.js
@@ -21,7 +21,7 @@ if (cluster.isMaster && !DEBUG) {
       BodyParser = require('body-parser'),
       phantom = require('phantom'),
       app = express(),
-      port = 8000,
+      port = 9263,
 
       WebpageProcessor = require('./components/WebpageProcessor'),
       ActionFactory = require('./components/ActionFactory'),
