8e972c00c70299b4504dfdb6e73edac331159f08
Rails to Cosmos <akatovda@yandex.com>
2016-09-06 17:37:05 +0300

    Implement elisp automatizations.

--- lisp/wpp.el
+++ lisp/wpp.el
@@ -9,4 +9,49 @@
   :kill-signal 'sigkill
   :kill-process-buffer-on-stop t)
 
+(defvar wpp-job-name)
+(defvar wpp-conf-path)
+(defvar wpp-sources)
+(defvar old-wpp-sources)
+(defvar new-wpp-sources)
+
+(setq wpp-conf-path "~/Documents/Stuff/wpp/configs/")
+(defun wpp-get-sources ()
+  (defun wpp-trim-json (str)
+    (subseq str 0 (- (length str) 5)))
+
+  (mapcar 'wpp-trim-json (directory-files wpp-conf-path nil ".*\.json")))
+
+(setq wpp-sources (wpp-get-sources))
+
+(defun wpp-send-config (config)
+  (if (not (equal config "__tank__"))
+      (let* ((bpr-process-directory wpp-conf-path)
+             (bpr-show-progress nil)
+             (bpr-close-after-success t))
+        (bpr-spawn
+         (concat "curl -H 'Content-Type: application/json' --data @" config ".json http://localhost:8283/ -o /tmp/" config ".json")))))
+
+(defun wpp-send ()
+  (interactive)
+
+  (setq new-wpp-sources (apply #'concat (wpp-get-sources)))
+  (if (not (equal new-wpp-sources old-wpp-sources))
+      (setq wpp-sources (wpp-get-sources)))
+  (setq old-wpp-sources new-wpp-sources)
+
+  (setq wpp-job-name (ido-completing-read "Run WebpageProcessor job: " wpp-sources))
+  (delete wpp-job-name wpp-sources)
+  (add-to-list 'wpp-sources wpp-job-name)
+
+  (if (equal wpp-job-name "__tank__")
+      (wpp-tank)
+    (wpp-send-config wpp-job-name))
+  (switch-to-buffer "*prodigy-wpp*"))
+
+(defun wpp-tank ()
+  (interactive)
+  (mapcar 'wpp-send-config wpp-sources))
+(bind-key "C-x RET" 'wpp-send)
+
 (provide 'wpp)
