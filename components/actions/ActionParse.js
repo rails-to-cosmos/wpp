d3544264129ecbc2e07459687761c024df9e099c
Rails to Cosmos <akatovda@yandex.com>
2016-09-06 14:42:41 +0300

    Parse bug fixes.

--- components/actions/ActionParse.js
+++ components/actions/ActionParse.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Action = require('./Action'),
     cheerio = require('cheerio'),
     get_page_content = require('../webpage/Utils').get_page_content,
@@ -6,17 +8,30 @@ var Action = require('./Action'),
 function ComplexSelector(selector) {
   this.selector = '';
   this.attribute = '';
+  this.index = -1;
 
-  if (!selector) {
-    return;
-  }
+  if (selector) {
+    // get index from selector
+    let eq_matches = selector.match(/:eq\((\d+)\)/);
+    if (eq_matches && eq_matches.length > 1) {
+      let index;
 
-  this.selector = selector;
+      try {
+        this.index = parseInt(eq_matches[1]);
+      } catch (exc) {
+        console.log('AttributeError: eq contains non-numeric value');
+      }
 
-  var sel_attr_list = selector.match(/(.*?)\[([a-zA-Z\-]+)\]/);
-  if (sel_attr_list) {
-    this.selector = sel_attr_list[1];
-    this.attribute = sel_attr_list[2];
+      selector = selector.replace(/:eq\((\d+)\)/, '');
+    }
+
+    let sel_attr_list = selector.match(/(.*?)\[([a-zA-Z\-]+)\]/);
+    if (sel_attr_list) {
+      this.selector = sel_attr_list[1];
+      this.attribute = sel_attr_list[2];
+    } else {
+      this.selector = selector;
+    }
   }
 }
 
@@ -35,23 +50,47 @@ ActionParse.prototype.main = function(subactions) {
     var selector = new ComplexSelector(ACTION.config.data.selector);
     var Representation = get_representation(selector);
     var pages = ACTION.get_from_store(ACTION.get_target());
-
     var parse_actions = pages.map(function(page) {
       return new Promise(function(resolveParse) {
         get_page_content(page, ACTION).then(function(content) {
           var result = [];
           var element, element_representation;
           var $ = cheerio.load(content);
+
           if (selector.selector[0] == '>') {
             var clean_selector = selector.selector.slice(1, selector.selector.length);
-            element = $(clean_selector).first();
+            if (selector.index > -1) {
+              try {
+                element = $(selector.selector).get(selector.index);
+              } catch (exc) {
+                console.log(exc);
+              }
+            } else {
+              element = $(clean_selector).first();
+            }
+
             element_representation = new Representation($, element, selector);
             result.push(element_representation.repr());
           } else if (selector.selector) {
-            $(selector.selector).each(function(id, el) {
-              element_representation = new Representation($, el, selector);
+            if (selector.index > -1) {
+              try {
+                element = $(selector.selector).get(selector.index);
+              } catch (exc) {
+                console.log(exc);
+              }
+
+              element_representation = new Representation($, element, selector);
               result.push(element_representation.repr());
-            });
+            } else {
+              try {
+                $(selector.selector).each(function(id, el) {
+                  element_representation = new Representation($, el, selector);
+                  result.push(element_representation.repr());
+                });
+              } catch (exc) {
+                console.log(exc);
+              }
+            }
           } else if (selector.attribute && !selector.selector) {
             element = $(content);
             element_representation = new Representation($, element, selector);
