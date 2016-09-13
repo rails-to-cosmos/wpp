9a8edd16bfea03bd9d922da00aeba7b7a1a7eff5
Rails to Cosmos <akatovda@yandex.com>
2016-09-13 21:28:36 +0300

    wpp path bug fix.

--- lisp/wpp.el
+++ lisp/wpp.el
@@ -4,7 +4,7 @@
   ;; :args '("wpp")
   :command "nodemon"
   :args '("--debug" "wpp")
-  :cwd "/Users/akatovda/Documents/Stuff/wpp/"
+  :cwd "~/Documents/Stuff/wpp/"
   :stop-signal 'kill
   :kill-signal 'sigkill
   :kill-process-buffer-on-stop t)
