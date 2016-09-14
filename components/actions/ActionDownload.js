f0f609289c36acff79c96d0eb29b02ee776a54aa
Rails to Cosmos <akatovda@yandex.com>
2016-09-14 14:36:04 +0300

    Remove logs.

--- components/actions/ActionDownload.js
+++ components/actions/ActionDownload.js
@@ -29,20 +29,14 @@ ActionDownload.prototype.main = function (subactions) {
 
   return new Promise(function(resolve, reject) {
     try {
-      ACTION.logger.info('1');
       let webpage = new Webpage(ACTION.get_browser());
-      ACTION.logger.info('2');
       webpage.create().then(function(page) {
-        ACTION.logger.info('3');
         try {
           let filters = ACTION.get_filters();
           Filters.applyOnPage(page, filters);
-          ACTION.logger.info('4');
           let url = ACTION.config.data.url;
           assert(url);
-          ACTION.logger.info('5');
           page.invokeAsyncMethod('open', url).then(function(status) {
-            ACTION.logger.info('6');
             try {
               get_page_content(page, ACTION).then(function(content) {
                 try {
