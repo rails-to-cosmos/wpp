076207ff9e9aefacc89e59986776bc2401d029c8
Rails to Cosmos <akatovda@yandex.com>
2016-09-19 18:27:55 +0300

    Do not close page on resource timeout.

--- components/webpage/Settings.js
+++ components/webpage/Settings.js
@@ -2,22 +2,24 @@
 
 var default_settings = (browser, page, config) => {
   page.setting('userAgent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11');
-  page.setting('resourceTimeout', 120000);
+  page.setting('resourceTimeout', 5000);
   page.property('viewportSize', {width: 800, height: 600});
 
   page.on('onResourceTimeout', function(e) {
-    console.log('PhantomJS request timed out');
-    console.log(e.errorCode);   // it'll probably be 408
-    console.log(e.errorString); // it'll probably be 'Network timeout on resource'
-    console.log('PhantomJS request timeout', e.url);         // the url whose request timed out
+    // console.log('PhantomJS request timed out');
+    // console.log(e.errorCode);   // it'll probably be 408
+    // console.log(e.errorString); // it'll probably be 'Network timeout on resource'
+    // console.log('PhantomJS request timeout', e.url);         // the url whose request timed out
 
-    let __free__ = function() {
-      page.stop();
-      page.close();
-      page = null;
-    };
+    // if (page) {
+    //   let __free__ = function() {
+    //     page.stop();
+    //     page.close();
+    //     page = null;
+    //   };
 
-    page.invokeMethod('clearMemoryCache').then(__free__, __free__);
+    //   page.invokeMethod('clearMemoryCache').then(__free__, __free__);
+    // }
   });
 
   page.on('onError', function() {
