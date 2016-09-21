15caa432087bcb14893f9e708ed3d2eb61edfddf
Rails to Cosmos <akatovda@yandex.com>
2016-09-21 16:58:07 +0300

    Rename unit test descriptions.

--- tests/DataCleaner.js
+++ tests/DataCleaner.js
@@ -5,37 +5,37 @@ let chai = require('chai'),
     DataCleaner = require('../components/webpage/DataCleaner');
 
 describe('DataCleaner', function() {
-  it('Replace multiple spaces with one', function() {
+  it('should replace multiple spaces with one', function() {
     let dc = new DataCleaner();
     expect(dc.clean('hello   hello')).to.equal('hello hello');
   });
 
-  it('Replace newlines with one space', function() {
+  it('should replace newlines with one space', function() {
     let dc = new DataCleaner();
     expect(dc.clean('hello\n\n\nhello')).to.equal('hello hello');
   });
 
-  it('Replace line breaks with one space', function() {
+  it('should replace line breaks with one space', function() {
     let dc = new DataCleaner();
     expect(dc.clean('hello\r\n\r\nhello')).to.equal('hello hello');
   });
 
-  it('Replace many escape symbols with one', function() {
+  it('should replace many escape symbols with one', function() {
     let dc = new DataCleaner();
     expect(dc.clean('\\\\\\\\\\\\')).to.equal('\\');
   });
 
-  it('Replace many eq symbols with one', function() {
+  it('should replace many eq symbols with one', function() {
     let dc = new DataCleaner();
     expect(dc.clean('===')).to.equal('=');
   });
 
-  it('Do not replace unicode characters', function() {
+  it('should not replace unicode characters', function() {
     let dc = new DataCleaner();
     expect(dc.clean('привет   привет')).to.equal('привет привет');
   });
 
-  it('Trim dirty string', function() {
+  it('should trim dirty string', function() {
     let dc = new DataCleaner();
     expect(dc.clean(' привет ')).to.equal('привет');
   });
