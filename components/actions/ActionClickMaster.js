e23103282448a882c0757f1ab65a272e40af5613
Rails to Cosmos <akatovda@yandex.com>
2016-10-12 16:59:30 +0300

    Major bug fix.

--- components/actions/ActionClickMaster.js
+++ components/actions/ActionClickMaster.js
@@ -24,55 +24,76 @@ ActionClickMaster.prototype.main = function (subactions) {
                     try {
                         let xpath_injection = new XPathInjection();
                         xpath_injection.apply(page).then(function() {
-                            const selector = ACTION.get_data().selector;
-                            page.evaluate(function(selector) {
-                                var result = [];
-                                var elements = document.querySelectorAll(selector);
-                                for (var ei=0; ei<elements.length; ei++) {
-                                    var xpel = window.__wpp__.xpath(elements[ei]);
-                                    result.push(xpel);
-                                }
-
-                                return result;
-                            }, selector).then(function(buttons) {
-                                let createSlaves = function() {
-                                    var slaves = [];
-                                    for (var index in buttons) {
-                                        var click_config = deepcopy(ACTION.config);
-                                        click_config.type = SLAVE_ACTION_TYPE;
-                                        click_config.name = ACTION.get_name() + '__' + index;
-                                        click_config.data = {
-                                            xpath: buttons[index]
-                                        };
-
-                                        var slave = ACTION.factory.create_action(click_config, ACTION);
-                                        slaves.push(slave);
+                            try {
+                                const selector = ACTION.get_data().selector;
+                                page.evaluate(function(selector) {
+                                    var result = [];
+                                    var elements = document.querySelectorAll(selector);
+                                    for (var ei=0; ei<elements.length; ei++) {
+                                        var xpel = window.__wpp__.xpath(elements[ei]);
+                                        result.push(xpel);
                                     }
 
-                                    return slaves;
-                                };
+                                    return result;
+                                }, selector).then(function(buttons) {
+                                    try {
+                                        let createSlaves = function() {
+                                            var slaves = [];
+                                            for (var index in buttons) {
+                                                var click_config = deepcopy(ACTION.config);
+                                                click_config.type = SLAVE_ACTION_TYPE;
+                                                click_config.name = ACTION.get_name() + '__' + index;
+                                                click_config.data = {
+                                                    xpath: buttons[index]
+                                                };
 
-                                let processSlaves = function(slaves) {
-                                    return new Promise(function(resolveSlave, rejectSlave) {
-                                        if (slaves.length == 0) {
-                                            resolveSlave();
-                                            return;
-                                        }
+                                                var slave = ACTION.factory.create_action(click_config, ACTION);
+                                                slaves.push(slave);
+                                            }
 
-                                        let slave_action = slaves.pop();
-                                        slave_action.main(subactions)
-                                            .then(ACTION.take_screenshot(page, 'click_after_' + slave_action.get_name()))
-                                            .then(processSlaves(slaves))
-                                            .then(resolveSlave)
-                                            .catch(rejectSlave);
-                                    });
-                                };
+                                            return slaves;
+                                        };
 
-                                processSlaves(createSlaves()).then(function() {
-                                    ACTION.finalize().then(resolve, reject);
-                                });
-                            }).catch(reject);
-                        }).catch(reject);
+                                        let processSlaves = function(slaves) {
+                                            return new Promise(function(resolveSlave, rejectSlave) {
+                                                try {
+                                                    if (slaves.length == 0) {
+                                                        resolveSlave();
+                                                        return;
+                                                    }
+
+                                                    let slave_action = slaves.pop();
+                                                    slave_action.main(subactions).then(function() {
+                                                        let promises = [
+                                                            ACTION.take_screenshot(page, 'click_after_' + slave_action.get_name()),
+                                                            processSlaves(slaves)
+                                                        ];
+
+                                                        Promise.all(promises).then(resolveSlave, rejectSlave);
+                                                    }, rejectSlave);
+                                                } catch (exc) {
+                                                    rejectSlave(exc);
+                                                }
+                                            });
+                                        };
+
+                                        ACTION.write_to_store(page);
+                                        let promises = [
+                                            ACTION.take_screenshot(page, 'click_before_click'),
+                                            processSlaves(createSlaves())
+                                        ];
+
+                                        Promise.all(promises).then(function() {
+                                            ACTION.finalize().then(resolve, reject);
+                                        }, reject);
+                                    } catch (exc) {
+                                        reject(exc);
+                                    }
+                                }, reject);
+                            } catch (exc) {
+                                reject(exc);
+                            }
+                        }, reject);
                     } catch (exc) {
                         reject(exc);
                     }
